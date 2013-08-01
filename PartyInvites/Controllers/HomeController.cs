3fd11a044ca7cf6873a7215579817f863b63f031
torazuka <torazuka@gmail.com>
2013-08-01 15:45:52 +0900

    Added the RsvpForm method for POST requests

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartyInvites.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,10 +19,17 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
         }
 
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: パーティ参加者にEmailを送る
+            return View("Thanks", guestResponse);
+        }
     }
 }
