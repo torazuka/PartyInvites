8f526299a97014be0a0f4245ff2ff4567c1a043c
torazuka <torazuka@gmail.com>
2013-08-01 16:14:11 +0900

    added validations on the model

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "名前を入力してください。")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Emailアドレスを入力してください。")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "正しいEmailアドレスを入力してください。")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "電話番号を入力してください。")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "出欠を決めてください。")]
         public bool? WillAttend { get; set; }
     }
 }
